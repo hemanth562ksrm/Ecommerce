b41769e330639cf76b641be32dd7413dd788d46b
HEMANTH KUMAR GOLLA <hemanthkumargolla242004@gmail.com>
2026-03-01 16:37:08 +0530

    Add product data to data.js

---
+++ Ecommerce-ui/js/data.js
@@ -0,0 +1,6 @@
+const products = [
+    { id: 1, name: "Samsung Mobile", price: 18000, category: "mobiles" },
+    { id: 2, name: "LG TV", price: 35000, category: "tv" },
+    { id: 3, name: "Dell Laptop", price: 55000, category: "electronics" },
+    { id: 4, name: "T-Shirt", price: 999, category: "fashion" }
+];
